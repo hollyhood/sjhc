9c27ef680295f15c87a2bc56597627a683425d2b
스정화이 <@DESKTOP-JO6QGDR>
2020-05-14 21:13:43 +0900

    별찍기

---
+++ Monster/src/star.java
@@ -0,0 +1,26 @@
+
+public class star {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=0; i<5; i++) { //i=0, i=1 ...
+			  for(int j=0; j<i+1; j++) { //j=1 ,j=2 ...
+			   System.out.print("*"); //*
+			  }
+			 System.out.println();
+			 }
+			 
+			 System.out.println("-----------");
+			 
+			 for(int i=0; i<5; i++) { //i=0, i=1, i=2 ...
+			  for(int j=0; j<5-i ; j++) { //j=5,j=4,j=3 ...
+			   System.out.print("*");
+			  }
+			  System.out.println();
+			 }
+			 System.out.println("-----------");
+
+
+	}
+
+}
