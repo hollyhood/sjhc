4105da6e33be1197184cfdbafac15ab3b2b6959a
스정화이 <@DESKTOP-JO6QGDR>
2020-05-14 21:10:22 +0900

    12321

---
+++ Monster/src/king.java
@@ -0,0 +1,17 @@
+
+public class king {
+
+	public static void main(String[] args) {
+		int hap=0;
+		int i;
+		
+		for(i=1;i<=10;i++) {
+			hap+=i;
+		}
+
+		System.out.printf("1에서 10까지의 합:%d\n",hap);
+	
+
+	}
+
+}
