a623d78f6da9034e44152d6e7db45f335e4e794e
스정화이 <@DESKTOP-JO6QGDR>
2020-06-05 00:37:06 +0900

    6월4일

---
+++ 6월4일 시작/src/고양이.java
@@ -0,0 +1,15 @@
+
+public class °í¾çÀÌ {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
